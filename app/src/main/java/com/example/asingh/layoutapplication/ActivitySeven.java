6fe9cc42d00363dd4c31b022c79943278cd85410
Avi Singh <iavi4@yahoo.com>
2015-10-08 10:00:07 -0400

    Added funtionality to access all layouts.

---
+++ app/src/main/java/com/example/asingh/layoutapplication/ActivitySeven.java
@@ -0,0 +1,15 @@
+package com.example.asingh.layoutapplication;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by 2016asingh on 10/8/2015.
+ */
+public class ActivitySeven extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout7);
+    }
+}
--- app/src/main/java/com/example/asingh/layoutapplication/MainActivity.java
+++ app/src/main/java/com/example/asingh/layoutapplication/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.asingh.layoutapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +14,62 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button1 = (Button) findViewById(R.id.button1);
+        button1.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), ActivityOne.class));
+            }
+        });
+
+        Button button2 = (Button) findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), ActivityTwo.class));
+            }
+        });
+
+        Button button3 = (Button) findViewById(R.id.button3);
+        button3.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), ActivityThree.class));
+            }
+        });
+
+        Button button4 = (Button) findViewById(R.id.button4);
+        button4.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), ActivityFour.class));
+            }
+        });
+
+        Button button5 = (Button) findViewById(R.id.button5);
+        button5.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), ActivityFive.class));
+            }
+        });
+
+        Button button6 = (Button) findViewById(R.id.button6);
+        button6.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), ActivitySix.class));
+            }
+        });
+
+        Button button7 = (Button) findViewById(R.id.button7);
+        button7.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), ActivitySeven.class));
+            }
+        });
+
+        Button button8 = (Button) findViewById(R.id.button8);
+        button8.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), ActivityEight.class));
+            }
+        });
     }
 
     @Override
